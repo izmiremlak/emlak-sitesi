6fdc42400435c3bd69e4be0210e0edfb1740ccd4
izmiremlak <izmiremlakbirligi@gmail.com>
2025-03-24 14:18:56 +0300

    Delete modules directory

--- modules/inc/anasayfa-meta.php
+++
@@ -1,9 +0,0 @@
-<!-- Meta Tags -->
-<title><?=$dayarlar->title;?></title>
-<meta name="keywords" content="<?=$dayarlar->keywords;?>" />
-<meta name="description" content="<?=$dayarlar->description;?>" />
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-<meta name="robots" content="All" />  
-<link rel="icon" type="image/x-icon" href="favicon.ico" />
-<!-- Meta Tags -->
