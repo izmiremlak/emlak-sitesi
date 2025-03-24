798f71f4d4b6dd35fb3b948664704d1736541ac1
izmiremlak <izmiremlakbirligi@gmail.com>
2025-03-24 15:21:52 +0300

    Add files via upload

---
+++ pages03/ekatalog_ekle.php
@@ -0,0 +1,92 @@
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h4 class="pull-left page-title">Yeni Katalog Ekle</h4>
+
+                        </div>
+                    </div>
+					
+                    <div class="row">
+                        
+                        <!-- Col 1 -->
+                        <div class="col-md-12">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+								
+									<div id="form_status"></div>
+                                    <form role="form" class="form-horizontal"  id="forms" method="POST" action="ajax.php?p=ekatalog_ekle" onsubmit="return false;" enctype="multipart/form-data">
+                                        
+                                        <div class="form-group">
+                                            <label for="baslik" class="col-sm-3 control-label">Başlık</label>
+                                            <div class="col-sm-9">
+											<input type="text" class="form-control" id="baslik" name="baslik" value="" placeholder="">
+                                        </div>
+                                        </div>
+										
+										<div class="form-group">
+                                            <label for="sira" class="col-sm-3 control-label">Sıra Nosu</label>
+                                            <div class="col-sm-9">
+											<input type="text" class="form-control" id="sira" name="sira" value="" placeholder="">
+                                        </div>
+                                        </div>
+										
+										<div class="form-group">
+                                            <label for="resim" class="col-sm-3 control-label">Listeleme Görseli</label>
+                                            <div class="col-sm-9">
+											<input type="file" class="form-control" id="resim" name="resim" value="" >
+											<p style="margin-left:10px;font-size:13px;margin-top:5px;">Yükleyeceğiniz görselin boyutları <?=$gorsel_boyutlari['ekatalog']['thumb_x']; ?> x <?=$gorsel_boyutlari['ekatalog']['thumb_y']; ?> px olmalıdır.</p>
+                                        </div>
+                                        </div>
+										
+										
+										<div class="form-group">
+                                            <label for="resim" class="col-sm-3 control-label">Dosya</label>
+                                            <div class="col-sm-9">
+											<input type="file" class="form-control" id="dosya" name="dosya" value="" >
+											
+                                        </div>
+                                        </div>
+										
+										
+										
+                                        
+									<div align="right">
+                                        <button type="submit" class="btn btn-purple waves-effect waves-light" onclick=" AjaxFormS('forms','form_status');">Kaydet</button>
+                                    </div>
+									
+									</form>
+									
+									
+									
+                                </div>
+                            </div>
+                        </div>
+						<!-- Col1 end -->
+						
+						</div><!-- row end -->
+						
+                        
+						 
+						
+					
+                    
+                    
+                    
+                    
+                    
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <script>
+        var resizefunc = [];
+    </script>
+	<script src="assets/js/admin.min.js"></script>
+	<link href="assets/plugins/notifications/notification.css" rel="stylesheet">
+    <script src="assets/vendor/notifyjs/dist/notify.min.js"></script>
+    <script src="assets/plugins/notifications/notify-metro.js"></script>
+    <script src="assets/plugins/notifications/notifications.js"></script>
+	
